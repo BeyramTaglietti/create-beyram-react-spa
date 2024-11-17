5b8db929aa97eda978d360f06a2c0414fe3afe69
Beyram <beyram.taglietti@gmail.com>
2024-11-17 10:08:33 +0100

    date configuration

---
+++ src/config/date.config.ts
@@ -0,0 +1,29 @@
+/**
+ * Date and time formats
+ * @type {Object}
+ */
+export const date_formats = {
+  /**
+   * Long date format (e.g., "01 Jan 2023")
+   * @example "01 Jan 2023"
+   */
+  LONG_DATE_FORMAT: "dd LLL y",
+
+  /**
+   * Short date format (e.g., "01/01/2023")
+   * @example "01/01/2023"
+   */
+  SHORT_DATE_FORMAT: "dd/MM/y",
+
+  /**
+   * Date format for API requests (e.g., "2023-01-01")
+   * @example "yyyy-MM-dd"
+   */
+  API_DATE_FORMAT: "yyyy-MM-dd",
+
+  /**
+   * Time format in 24-hour format (e.g., "23:59")
+   * @example "23:59"
+   */
+  TIME_FORMAT: "HH:mm",
+};
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,3 @@
 export * from "./axios.config";
+export * from "./date.config";
+
