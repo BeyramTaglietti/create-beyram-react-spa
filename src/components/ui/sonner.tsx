1686e3253a37939cd82ef064aa1da00395f0857f
BeyramTaglietti <beyram.taglietti@gmail.com>
2025-02-27 10:30:40 +0100

    fix: sonner custom styles

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -3,23 +3,18 @@ import { cva } from "class-variance-authority";
 import { useTheme } from "next-themes";
 import { Toaster as Sonner, ToasterProps } from "sonner";
 
-const toastVariants = cva(
-  "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-  {
-    variants: {
-      variant: {
-        default:
-          "group-[.toaster]:bg-primary group-[.toaster]:text-primary-foreground",
-        destructive:
-          "group-[.toaster]:bg-destructive group-[.toaster]:text-destructive-foreground",
-        success: "group-[.toaster]:bg-green-500 group-[.toaster]:text-white",
-      },
+const toastVariants = cva("", {
+  variants: {
+    variant: {
+      default: "!bg-background !text-foreground !border-background",
+      error: "!bg-destructive !text-white !border-destructive",
+      success: "!bg-green-500 !text-white !border-green-500",
     },
-    defaultVariants: {
-      variant: "default",
-    },
-  }
-);
+  },
+  defaultVariants: {
+    variant: "default",
+  },
+});
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme();
@@ -30,13 +25,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
       className="toaster group"
       toastOptions={{
         classNames: {
-          description: "group-[.toast]:text-muted-foreground",
-          actionButton:
-            "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
-          cancelButton:
-            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
-          toast: cn(toastVariants({ variant: "default" })),
-          error: cn(toastVariants({ variant: "destructive" })),
+          default: cn(toastVariants({ variant: "default" })),
+          error: cn(toastVariants({ variant: "error" })),
           success: cn(toastVariants({ variant: "success" })),
         },
       }}
--- src/pages/Index.page.tsx
+++ src/pages/Index.page.tsx
@@ -1,13 +1,18 @@
 import { Button } from "@/components/ui";
 import { useTranslation } from "react-i18next";
+import { toast } from "sonner";
 
 export const IndexPage = () => {
   const { t } = useTranslation();
 
   return (
     <>
-      <h1 className="text-red-500">{t("app.home")}</h1>
-      <Button>Click me</Button>
+      <div className="w-dwh h-dvh flex items-center justify-center flex-col gap-2">
+        <h1>{t("app.home")}</h1>
+        <Button onClick={() => toast.success("This is a success toast")}>
+          Show a toast
+        </Button>
+      </div>
     </>
   );
 };
