92ea83d80be8334c9f7cf6be00b973b7f051b213
BeyramTaglietti <beyram.taglietti@gmail.com>
2025-06-06 19:46:46 +0200

    feat: support cloning inside current directory

---
+++ README.md
@@ -0,0 +1,69 @@
+# This is my React SPA boilerplate
+
+This is a boilerplate for a React Single Page Application with amazing tools and libraries always up to date.
+
+<div style="display:flex; justify-content:space-between; align-items:center">
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vitejs-logo.svg/1039px-Vitejs-logo.svg.png" width=50 height=50/>
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/React_Logo_SVG.svg/1200px-React_Logo_SVG.svg.png" width=56 height=50/>
+<img src="https://avatars.githubusercontent.com/u/139895814?v=4" width=56 height=50/>
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2560px-Tailwind_CSS_Logo.svg.png" width=70 height=50/>
+<img src="https://tanstack.com/assets/splash-dark-8nwlc0Nt.png" width=50 height=50/>
+<img src="https://avatars.githubusercontent.com/u/8546082?s=280&v=4" width=50 height=50/>
+</div>
+
+<br/>
+
+## To use this boilerplate
+
+```js
+npx create-beyram-react-spa
+// or
+pnpm dlx create-beyram-react-spa
+// or
+bunx create-beyram-react-spa
+```
+
+---
+
+### Bundler
+
+- Vite
+
+### Styling
+
+- TailwindCSS
+- Shadcn
+
+### Router
+
+- Tanstack Router
+
+### Queries
+
+- Tanstack Query
+- Axios
+
+### I18n
+
+- React-i18next
+- i18next
+
+### Dates
+
+- Date-fns
+
+---
+
+### Complete list of dependencies
+
+- tailwindcss
+- tanstack router
+- tanstack query
+- react-i18next
+- i18next
+- date-fns
+- axios
+- shadcn/ui
+- sonner
+- @julr/vite-plugin-validate-env
+- husky
--- bin/create-spa.mjs
+++ bin/create-spa.mjs
@@ -8,68 +8,87 @@ if (process.argv.length < 3) {
   console.log("You have to provide a name to your app.");
   console.log("For example :");
   console.log("    npx create-beyram-react-spa my-app");
+  console.log("    npx create-beyram-react-spa .");
   process.exit(1);
 }
 
-const projectName = process.argv[2];
+let projectName = process.argv[2];
 const currentPath = process.cwd();
-const projectPath = path.join(currentPath, projectName);
-const git_repo = "https://github.com/BeyramTaglietti/beyram-react-spa";
+let projectPath;
 
-try {
-  fs.mkdirSync(projectPath);
-} catch (err) {
-  if (err.code === "EEXIST") {
-    console.log(
-      `The file ${projectName} already exist in the current directory, please give it another name.`
-    );
-  } else {
-    console.log(error);
+if (projectName === ".") {
+  projectPath = currentPath;
+  projectName = path.basename(currentPath);
+  console.log(`Using current directory: ${projectName}`);
+} else {
+  projectPath = path.join(currentPath, projectName);
+  // Create the project directory
+  try {
+    fs.mkdirSync(projectPath);
+  } catch (err) {
+    if (err.code === "EEXIST") {
+      console.log(
+        `The file ${projectName} already exists in the current directory, please give it another name.`
+      );
+    } else {
+      console.log(err);
+    }
+    process.exit(1);
   }
-  process.exit(1);
 }
+
+const git_repo = "https://github.com/BeyramTaglietti/beyram-react-spa";
+const tempDir = path.join(currentPath, `__temp_${projectName}`);
 
 async function main() {
   try {
     console.log("Downloading files...\n");
-    execSync(`git clone --depth 1 --branch master ${git_repo} ${projectPath}`);
+
+    execSync(`git clone --depth 1 --branch master ${git_repo} ${tempDir}`);
+
+    // Copy template/default contents to the project directory
+    const templatePath = path.join(tempDir, "template/default");
+
+    if (fs.existsSync(templatePath)) {
+      // Copy all contents from template/default to project directory
+      fs.cpSync(templatePath, projectPath, { recursive: true });
+      console.log("Template files copied successfully");
+    } else {
+      console.log("Template directory not found. Aborting installation.");
+      process.exit(1);
+    }
+
+    // Clean up the temporary directory
+    fs.rmSync(tempDir, { recursive: true, force: true });
 
     process.chdir(projectPath);
 
-    console.log("\nRemoving useless files");
-
-    const gitPath = path.join(projectPath, ".git");
-    if (fs.existsSync(gitPath)) {
-      fs.rmSync(gitPath, { recursive: true, force: true });
-      console.log("Removed .git directory");
-    } else {
-      console.log(".git directory not found. Skipping removal.");
-    }
-
     console.log("\nSetting up package.json");
     const packageJsonPath = path.join(projectPath, "package.json");
-    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
-    packageJson.name = projectName;
-    packageJson.version = "0.0.0";
-    delete packageJson.bin;
-
-    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-
-    const binPath = "bin";
-    if (fs.existsSync(binPath)) {
-      fs.rmSync(path.join(projectPath, binPath), { recursive: true });
-      console.log("Removed bin directory");
+    if (fs.existsSync(packageJsonPath)) {
+      const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
+      packageJson.name = projectName;
+      fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
     } else {
-      console.log("bin directory not found. Skipping removal.");
+      console.log("package.json not found in template");
     }
 
     console.log("\nThe installation is done\n");
     console.log("You can now do the following:\n");
-    console.log(`    cd ${projectName}`);
+
+    if (projectPath !== currentPath) {
+      console.log(`    cd ${projectName}`);
+    }
+
     console.log("    pnpm install");
     console.log("    code .\n");
   } catch (error) {
     console.log(error);
+    // Clean up if something went wrong
+    if (fs.existsSync(tempDir)) {
+      fs.rmSync(tempDir, { recursive: true, force: true });
+    }
   }
 }
+
 main();
